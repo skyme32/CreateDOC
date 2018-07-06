594bb9103d78232b51415056b78105dc01518579
skyme32 <mmejiasg@zoho.com>
2018-07-06 14:16:51 +0200

    Create the job class.
    Signed-off-by: skyme32 <mmejiasg@zoho.com>

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,9 +5,11 @@ import java.io.File;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
@@ -20,16 +22,20 @@ public class Controller {
 
     private Object Stage;
 
+    @FXML
+    private TextField inputText;
+
+
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
-
-
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File selectedDirectory = directoryChooser.showDialog((Window) Stage);
+        /*TextField test = new T;*/
 
         if(selectedDirectory == null){
             System.out.println("No Directory selected");
         }else {
             System.out.println(selectedDirectory.getAbsolutePath());
+            inputText.setText(selectedDirectory.getAbsolutePath());
         }
     }
 }
