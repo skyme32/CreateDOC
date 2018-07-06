bf77895868069cfc399b5674337f3e5410982ef2
skyme32 <mmejiasg@zoho.com>
2018-07-06 13:53:12 +0200

    Create the job class.
    Signed-off-by: skyme32 <mmejiasg@zoho.com>

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,25 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class Controller {
 
+    private Object Stage;
+
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
 
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showOpenDialog((Window) Stage);
+
+        System.out.println(fileChooser.getTitle());
+        System.out.println(fileChooser.getExtensionFilters());
+        System.out.println(fileChooser.getInitialDirectory());
+        System.out.println(fileChooser.getInitialFileName());
+        System.out.println(fileChooser.getSelectedExtensionFilter());
     }
 }
